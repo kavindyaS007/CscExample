acf63807a881bcc60c36ab445fb90db590f13ade
kavindyaS007 <kavindyasamarsinghe@gmail.com>
2020-09-18 05:04:20 +0530

    getters and setters

--- CscExamples.java
+++ CscExamples.java
@@ -4,23 +4,23 @@ public class CscExamples{
 	String text;
 	
 	public void setQuestionNo(int questionNo){
-		
+		this.questionNo = questionNo;
 	}
 	public void setCategory(String category){
-		
+		this.category = category;
 	}
 	public void setText(String text){
-		
+		this.text = text;
 	}
 	
 	public int getQuestionNo(){
-		
+		return questionNo;
 	}
 	public String getCategory(){
-		
+		return category;
 	}
-	public String getQuestionNo(){
-		
+	public String getText(){
+		return text;
 	}
 	
 	public void addQuestion(CscExamples quiz){
