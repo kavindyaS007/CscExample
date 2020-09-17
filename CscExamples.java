d76204c6880a94ab64d3d0373b57050cbeaea910
kavindyaS007 <kavindyasamarsinghe@gmail.com>
2020-09-18 04:42:03 +0530

    mainCode

---
+++ CscExamples.java
@@ -0,0 +1,35 @@
+public class CscExamples{
+	int questionNo;
+	String category;
+	String text;
+	
+	public void setQuestionNo(int questionNo){
+		
+	}
+	public void setCategory(String category){
+		
+	}
+	public void setText(String text){
+		
+	}
+	
+	public int getQuestionNo(){
+		
+	}
+	public String getCategory(){
+		
+	}
+	public String getQuestionNo(){
+		
+	}
+	
+	public void addQuestion(CscExamples quiz){
+		
+	}
+	public  void removeQuestion(int qId){
+		
+	}
+	public void editQuestion(int qId){
+		
+	}
+}
